5539ad8c98794786c2f403b1008fa4dae8d42fd3
p4panash <muntean.catalin.avram@gmail.com>
2022-08-26 09:28:34 +0300

    Add navigation stack

---
+++ src/navigation/MainNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const MainNavigator = (): React.ReactElement => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainNavigator;
