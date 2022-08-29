4c0576a01a55c3aeab0e42e5b80056b775864dae
p4panash <muntean.catalin.avram@gmail.com>
2022-08-29 20:33:08 +0300

    Include author data

--- src/api/manga.tsx
+++ src/api/manga.tsx
@@ -1,22 +1,37 @@
 import request from '../utils/request';
 
+const qs = require('qs');
+
 const parseMangaData = (mangaData: any) => {
   const fileName = mangaData.relationships.find(
     (data: any) => data.type === 'cover_art',
   ).attributes.fileName;
   const coverURI = `https://uploads.mangadex.org/covers/${mangaData.id}/${fileName}`;
 
-  return {id: mangaData.id, attributes: mangaData.attributes, cover: coverURI};
+  const authorName = mangaData.relationships.find(
+    (data: any) => data.type === 'author',
+  ).attributes.name;
+
+  return {
+    id: mangaData.id,
+    attributes: mangaData.attributes,
+    cover: coverURI,
+    author: authorName,
+    year: mangaData.year,
+  };
 };
 
 export const getMangaList = async (offset: Number = 0) => {
   return request({
     url: 'manga',
     params: {
-      'contentRating[]': 'safe',
+      contentRating: ['safe'],
       offset: offset,
       'order[latestUploadedChapter]': 'desc',
-      'includes[]': 'cover_art',
+      includes: ['cover_art', 'author'],
+    },
+    paramsSerialzer: params => {
+      return qs.stringify(params);
     },
   }).then(result => {
     const newResult = result.data.map((value: any) => {
